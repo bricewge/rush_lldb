a88d08dfa0d0c1e03a7bd722db057f3f40cb707a
Brice Waegeneire <brice.wge@gmail.com>
2017-04-29 15:11:51 +0200

    initial commit

---
+++ 02-script/reverse.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+
+import lldb
+
+def reverse(debugger, command, result, internal_dict):
+   print >>result, ("FT_" + str(lldb.target)[::-1])
+
+def __lldb_init_module(debugger, internal_dict):
+    debugger.HandleCommand('command script add -f reverse.reverse reverse')
